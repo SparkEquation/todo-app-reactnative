cd392ef7190cbeac5acf55248a3f1dae4b41899b
dmitry.alfimov <dmitry.alfimov@sparkequation.com>
2018-09-12 10:58:22 +0700

    add persist store

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,16 +1,19 @@
 import { createStore } from 'redux'
 import rootReducer from '../reducers'
 
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    stateReconciler: autoMergeLevel2 // see "Merge Process" section for details.
+};
+
 export default function configureStore(preloadedState) {
-  const store = createStore(rootReducer, preloadedState)
-
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../reducers', () => {
-      const nextReducer = require('../reducers').default
-      store.replaceReducer(nextReducer)
-    })
-  }
-
+  const pReducer = persistReducer(persistConfig, rootReducer);
+  const store = createStore(pReducer);
+  const persistor = persistStore(store);
   return store
 }
